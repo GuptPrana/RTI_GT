cea63f154e6a3dc1a921764d2295b73912f6250b
Gupt Prana <68948549+GuptPrana@users.noreply.github.com>
2024-07-23 18:22:26 +0800

    saving logic

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ Camera Setup: Intel Realsense D435F
 > References for RealSense Python Wrapper: 
 > - https://pypi.org/project/pyrealsense2/
 > - https://github.com/IntelRealSense/librealsense/tree/master/wrappers/python
-
--- realsense.py
+++ realsense.py
@@ -2,17 +2,19 @@ import pyrealsense2 as rs
 import datetime
 import numpy as np
 import cv2
+import os
 
 pipeline = rs.pipeline()
 config = rs.config()
 
+# 6FPS seems to be lowest 
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 6)
 # for depth, rs.stream.depth
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-
 pipeline.start(config)
+cwd = os.getcwd()
 
 while True: 
-    frame_object = pipeline.wait_for_frame()
+    frame_object = pipeline.wait_for_frames()
     # for depth image, frame_object.get_depth_frame()
     color_frame = frame_object.get_color_frame()
     color_image = np.asanyarray(color_frame.get_data())
@@ -20,10 +22,11 @@ while True:
     # save frame with local timestamp
     timestamp = datetime.datetime.now().time()
     # hh:mm:ss.xxxxxx
-    filename = 'data/' + str(timestamp).replace('.', ':') + '.jpg'
+    filename = str(timestamp).replace(':', '_').replace('.', '_') + '.jpg'
+    filepath = os.path.join(cwd, 'data', filename)
 
     # need BGR for cv2
-    cv2.imwrite(filename, color_image)
+    cv2.imwrite(filepath, color_image)
     cv2.imshow('RGB', color_image)
 
     # press 'q' to exit
