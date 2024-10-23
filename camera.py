c8e7ff3f6e78134b8624f3097f67b68b00b5a06c
Gupt Prana <68948549+GuptPrana@users.noreply.github.com>
2024-10-23 22:52:36 +0800

    column_stack

--- camera.py
+++ camera.py
@@ -21,21 +21,25 @@ class Camera:
     return R @ P @ Y
   
   def transformation_matrix(self):
-    transform = np.hstack((self.rotation_matrix(), np.array(self.translation)))
+    transform = np.column_stack((self.rotation_matrix(), np.array(self.translation)))
     return np.vstack((transform, np.array([0, 0, 0, 1])))
 
   def inverse_transformation_matrix(self):
     # return inv(self.transformation_matrix())
     rotation_t = self.rotation_matrix().transpose()
-    translation_t = - self.rotation_matrix.transpose() @ np.array(self.translation)
-    return np.vstack((np.hstack((rotation_t, translation_t)), np.array([0, 0, 0, 1]))) 
-  
-  def transform_pointclouds(self, pointcloud, transformation_matrix):
+    translation_t = - self.rotation_matrix().transpose() @ np.array(self.translation)
+    return np.vstack((np.column_stack((rotation_t, translation_t)), np.array([0, 0, 0, 1]))) 
+
+  def transform_pointcloud(self, pointcloud, transformation_matrix):
     # transformation_matrix = C1.transformation_matrix() to transform pointcloud to C1 coordinate space. 
     points = np.asarray(pointcloud.points)
     # Homogeneous Coordinates: [X, Y, Z, 1]
-    homogeneous_points = np.hstack((points, np.ones((points.shape[0], 1))))
+    homogeneous_points = np.column_stack((points, np.ones((points.shape[0], 1))))
     inverse_transformed = (self.inverse_transformation_matrix() @ homogeneous_points.transpose()).transpose()
     transformed_points = (transformation_matrix @ inverse_transformed.transpose()).transpose()[:, :3]
+    # print(transformation_matrix)
+    # print(self.inverse_transformation_matrix())
     # Need to read the array in o3d.geometry.PointCloud() object
     return transformed_points
+
+  ## Can add intrinsic calibration codes here too
