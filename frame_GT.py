e059267d1b03ce1031ec2ccc30c59cdb1f3da1af
Gupt Prana <68948549+GuptPrana@users.noreply.github.com>
2025-07-17 01:48:15 +0800

    use GMM for object segmentation

--- frame_GT.py
+++ frame_GT.py
@@ -3,36 +3,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import ConvexHull
 from shapely.geometry import Polygon
-from sklearn.cluster import DBSCAN
+from sklearn.mixture import GaussianMixture
 
 
-def segment(points, plot=False):
-    # points = [v1, v2, v3, v4]
-    model = DBSCAN(eps=10, min_samples=5).fit(points)
-    labels = model.labels_
-    clusters = np.unique(labels[labels != -1])
+def segment(points, object_count=2, plot=False):
+    gmm = GaussianMixture(n_components=object_count)
+    gmm.fit(points)
+    labels = gmm.predict(points)
+    clusters = np.unique(labels)
     segmented_points = [points[labels == cluster] for cluster in clusters]
 
     if plot:
         plt.figure(figsize=(10, 10))
-        clusters = set(labels)
         colors = ["red", "blue", "green", "orange"]
 
-        for label in clusters:
-            if label == -1:
-                color = "gray"
-            else:
-                color = colors[label % len(colors)]
-            cluster_mask = labels == label
+        for cluster in clusters:
+            mask = labels == cluster
+            color = colors[cluster % len(colors)]
             plt.scatter(
-                points[cluster_mask, 0],
-                points[cluster_mask, 1],
+                points[mask, 0],
+                points[mask, 1],
                 c=color,
                 s=20,
-                label=f"{label}",
+                label=f"{cluster}",
             )
 
-        plt.title("DBSCAN")
+        plt.title("GMM Clustering")
         plt.xlim(0, 224)
         plt.ylim(0, 224)
         plt.legend()
@@ -42,13 +38,18 @@ def segment(points, plot=False):
     return segmented_points
 
 
-def make_gt(segmented_points, frame_size=224, plot=False):
-    gt = np.zeros((frame_size, frame_size), dtype=np.uint8)
+def object_shape(points):
+    # try alpha shape (convex hull may be distorted due to outlier points)
+    # or smoothen in preprocessing
+    hull = ConvexHull(points)
+    hull_points = points[hull.vertices]
+    return Polygon(hull_points)
 
-    for item in segmented_points:
-        hull = ConvexHull(item)
-        hull_points = item[hull.vertices]
-        shape = Polygon(hull_points)
+
+def make_gt(segmented_points, image_size=224, plot=False):
+    gt = np.zeros((image_size, image_size), dtype=np.uint8)
+    for points in segmented_points:
+        shape = object_shape(points)
         mask = np.array(shape.exterior.coords).round().astype(np.int32)
         mask = mask.reshape((-1, 1, 2))
         cv2.fillPoly(gt, [mask], 1)
@@ -60,4 +61,4 @@ def make_gt(segmented_points, frame_size=224, plot=False):
 
 
 def motion_compensated_gt(gt_frames):
-    pass
+    pass  # return np.logical_or.reduce(gt_frames)
