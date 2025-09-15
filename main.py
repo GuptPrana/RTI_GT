7cd9a543e15eceb0e02e36cc06f946e2699fbc53
Gupt Prana <68948549+GuptPrana@users.noreply.github.com>
2025-09-15 15:22:13 +0800

    Debugging edge bleeding

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 
-from config import Config, RTI_Config
+from config import Config, GT_Config, RTI_Config
 from masks.format_pcd import *
 from masks.frame_cmask import make_buffer_mask, make_final_cmask
 from pose import linear_timescale
@@ -39,7 +39,7 @@ def test_cases(timestamps, filenames):
 
 
 # Timestamp-based frame alignment
-def align_timestamps(config, rti_config, **kwargs):
+def align_timestamps(config, rti_config=None, **kwargs):
     datapaths = [
         os.path.join(config.datafolder, f"camera_{view}", config.input_filetype)
         for view in range(config.num_cameras)
@@ -52,15 +52,21 @@ def align_timestamps(config, rti_config, **kwargs):
     filetype2 = kwargs.get("filetype2", "npy")
     eps = kwargs.get("eps", 1e6)
 
-    if rti_config.rti_datapath is None:
-        timestamps = []
+    if rti_config is None:
+        timestamps = [
+            linear_timescale(
+                path1=config.datapaths[0],
+                ref1=config.start_end_ref[0],
+                filetype1=filetype1,
+            )
+        ]
         for idx in range(1, len(datapaths)):
             timestamps.append(
                 linear_timescale(
-                    config.datapaths[0],
-                    config.start_end_ref[0],
-                    config.datapaths[idx],
-                    config.start_end_ref[idx],
+                    path1=config.datapaths[0],
+                    ref1=config.start_end_ref[0],
+                    path2=config.datapaths[idx],
+                    ref2=config.start_end_ref[idx],
                     filetype1=filetype1,
                     filetype2=filetype2,
                     eps=eps,
@@ -71,10 +77,10 @@ def align_timestamps(config, rti_config, **kwargs):
         for idx in range(len(datapaths)):
             timestamps.append(
                 linear_timescale(
-                    rti_config.rti_datapath,
-                    rti_config.rti_start_end_ref[0],
-                    datapaths[idx],
-                    config.start_end_ref[idx],
+                    path1=rti_config.rti_datapath,
+                    ref1=rti_config.rti_start_end_ref[0],
+                    path2=datapaths[idx],
+                    ref2=config.start_end_ref[idx],
                     filetype1=filetype1,
                     filetype2=filetype2,
                     eps=eps,
@@ -87,7 +93,7 @@ def align_timestamps(config, rti_config, **kwargs):
         print("NaN present")
         keep = ~np.any(np.isnan(timestamps), axis=0)
         timestamps = timestamps[:, keep]
-        kept_indices = np.where(keep)[0]
+        kept_indices = np.where(keep)[0][0]
 
     if config.path_to_save_timestamps:
         np.save(config.path_to_save_timestamps, timestamps.T)
@@ -114,10 +120,9 @@ def create_dataset(config, timestamps):
     os.makedirs(os.path.join(config.plt_dir, config.datafolder), exist_ok=True)
 
     DOI_planes, affine_matrices = precompute_constants(config)
-    buffer_mask = make_buffer_mask(
-        config.image_size, config.buffer, config.buffer_corner
-    )
+    buffer_mask = make_buffer_mask(config.image_size)
 
+    block = 5000 if not config.save_masks else 1
     rows = tqdm(timestamps)
     for row in rows:
         all_points = []
@@ -134,7 +139,7 @@ def create_dataset(config, timestamps):
                 affine_matrices[view],
                 cropped_points,
                 DOI_size=config.DOI_size,
-                buffer=config.buffer,
+                buffer=GT_Config.buffer,
                 image_size=config.image_size,
             )
             all_points.append(points)
@@ -145,26 +150,28 @@ def create_dataset(config, timestamps):
             buffer_mask=buffer_mask,
             object_count=config.object_count,
             image_size=config.image_size,
-            alpha=config.object_alpha,
             plot=config.plot,
         )
 
         # ignore corrupt GT
         if not isinstance(gt, np.ndarray):
-            print("Error GT")
+            print(f"Error GT: {row[0]}")
             continue
 
-        gt_path = os.path.join(
-            config.gt_dir, config.datafolder, str(row[0]) + "." + config.output_filetype
-        )
-        cmask_path = os.path.join(
-            config.cmask_dir,
-            config.datafolder,
-            str(row[0]) + "." + config.output_filetype,
-        )
-        # cv2 and np set (0, 0) at top left by default (origin upper).
-        cv2.imwrite(gt_path, gt * 255)
-        cv2.imwrite(cmask_path, cmask * 255)
+        if config.save_masks:
+            gt_path = os.path.join(
+                config.gt_dir,
+                config.datafolder,
+                str(row[0]) + "." + config.output_filetype,
+            )
+            cmask_path = os.path.join(
+                config.cmask_dir,
+                config.datafolder,
+                str(row[0]) + "." + config.output_filetype,
+            )
+            # cv2 and np set (0, 0) at top left by default (origin upper).
+            cv2.imwrite(gt_path, gt * 255)
+            cv2.imwrite(cmask_path, cmask * 255)
         rows.set_description(f"Prepared GT for {config.datafolder}/{row[0]}")
 
         if config.save_2D_points:
@@ -193,7 +200,7 @@ def create_dataset(config, timestamps):
             plt.close()
 
         cv2.imshow("", gt * 255)
-        key = cv2.waitKey(1)
+        key = cv2.waitKey(block)
         if key == 27:  #  ESC to quit
             break
 
@@ -205,6 +212,7 @@ if __name__ == "__main__":
         datafolder="realsense_data_306_b",
         gt_dir=os.path.join("images", "gt"),
         cmask_dir=os.path.join("images", "cmask"),
+        plt_dir=os.path.join("images", "plt"),
         object_count=2,  # objects in DOI
         image_size=112,  # in pixels
         DOI_size=3,  # DOI square length in meters
@@ -213,13 +221,13 @@ if __name__ == "__main__":
 
     # Camera positions [x, y] in global coordinates
     # Must ensure camera_{k} is k-th entry in cameras
-    cameras = np.array([[3.73, 1.5], [1.5, 3.8], [-0.6, 1.5], [1.5, -0.85]])
+    # cameras = np.array([[3.73, 1.5], [1.5, 3.8], [-0.6, 1.5], [1.5, -0.85]])
+    cameras = np.array([[3.73, 1.5], [1.5, -0.85], [-0.6, 1.5], [1.5, 3.8]])
     cameras = cameras * (config.image_size / config.DOI_size)
     config.cameras = np.round(cameras, 1)
 
     # Corners of DOI plane in meters, ensure correct order
     config.dst_pts = np.array([[3, 3], [0, 3], [0, 0], [3, 0]])
-    config.buffer = int(0.1 * config.image_size)
 
     rti_config = RTI_Config()
     # Reference start and end frames
@@ -229,10 +237,9 @@ if __name__ == "__main__":
         2: [56836339960, 59354071415],
         3: [56836362644, 59354092562],
     }
-    config.path_to_save_timestamps = "timestamps.npy"
-    config.kept_indices_path = "kept_indices.npy"
+    # config.path_to_save_timestamps = "timestamps.npy"
+    # config.kept_indices_path = "kept_indices.npy"
     timestamps = align_timestamps(config, rti_config)
-
     # Picked Points Path (Corners)
     config.picked_points_paths = [
         os.path.join("constants", f"picked_points_{view}.npy")
@@ -248,4 +255,22 @@ if __name__ == "__main__":
     ]
     config.intrinsics_list = prepare_intrinsics(intrinsics_paths)
 
+    # Uncomment following block for debugging.
+    # timestamps = align_timestamps(config)
+    # print(timestamps.shape)
+    # config.datafolder = "realsense_data_306_b"
+    # config.gt_dir = os.path.join("test", "gt")
+    # config.cmask_dir = os.path.join("test", "cmask")
+    # config.plt_dir = os.path.join("test", "plt")
+    # config.save_2D_points = True
+    # config.plot = True
+    # filenames = [
+    #     56995060462,
+    #     56992593854,
+    #     57375961923,
+    #     57032290327,
+    # ]
+    # timestamps = test_cases(timestamps, filenames)
+
+    # Comment following line when debugging.
     create_dataset(config, timestamps)
